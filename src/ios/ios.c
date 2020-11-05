4387925631ea78920aaabf3efc15d29177c55c16
KrimtonZ <krimtonz@gmail.com>
2020-11-05 15:01:44 -0600

    split out asm files

---
+++ src/ios/ios.c
@@ -0,0 +1,29 @@
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B884C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8964.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8A84.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8B44.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8BEC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8CEC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8DF4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8EF4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B8FFC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B90DC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B91C4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B92FC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B942C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B9568.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/ios/ios/func_800B964C.s")
