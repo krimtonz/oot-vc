fa4b9d6ba68133576d02711369393a04540f062f
KrimtonZ <krimtonz@gmail.com>
2021-05-28 05:53:13 -0500

    split out library files

---
+++ include/library.h
@@ -0,0 +1,6 @@
+#ifndef _LIBRARY_H
+#define _LIBRARY_H
+
+#include "types.h"
+
+#endif
---
+++ src/virtual_console/library.c
@@ -0,0 +1,17 @@
+#include "library.h"
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005B7A0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005BAC0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005C1F4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/libraryTestFunction.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005CFEC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005D554.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005D614.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/library/func_8005D6FC.s")
